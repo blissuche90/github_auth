e9614809641553c503da8398c6bada46239657e8
Bliss Uche <uchechukwunjoku.uchenna@gmail.com>
2020-04-23 22:21:15 +0100

    update keys

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	development: {
-		db: 'mongodb://blissuche:1uchenna@ds049864.mlab.com:49864/gihub-auth',
+		db: 'mongodb://localhost',
 		app: {
 			name: 'FireFlies GitHub Auth Sample'
 		},
@@ -16,13 +16,13 @@ module.exports = {
 			callbackURL: "{{PLACEHOLDER}}"
 		},
 		github: {
-			clientID: "68251764f47083a1de55",
-			clientSecret: "5780eb8e1640c4c1d913ea6c5beb8ba8eae0cd1e",
-			callbackURL: "https://fireflies-git-auth.herokuapp.com/auth/github/callback"
+			clientID: "{{PLACEHOLDER}}",
+			clientSecret: "{{PLACEHOLDER}}",
+			callbackURL: "{{PLACEHOLDER}}"
 		}
 	},
   	production: {
-    	db: 'mongodb://blissuche:1uchenna@ds049864.mlab.com:49864/gihub-auth',//process.env.MONGOLAB_URI || process.env.MONGOHQ_URL,
+    	db: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL,
 		app: {
 			name: 'FireFlies GitHub Auth Sample'
 		},
@@ -37,9 +37,9 @@ module.exports = {
 			callbackURL: ''
 		},
 		github: {
-			clientID: '68251764f47083a1de55',
-			clientSecret: '5780eb8e1640c4c1d913ea6c5beb8ba8eae0cd1e',
-			callbackURL: 'https://fireflies-git-auth.herokuapp.com/auth/github/callback'
+			clientID: '{{PLACEHOLDER}}',
+			clientSecret: '{{PLACEHOLDER}}',
+			callbackURL: '{{PLACEHOLDER}}'
 		}
  	}
 }
